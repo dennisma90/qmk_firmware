ee12f024c14eb2b923785685610eb159731b578d
dennisma90 <dennis.mannstrom@gmail.com>
2022-03-20 13:23:56 +0100

    Switched thumb keys

--- keyboards/ferris/keymaps/colemakDH/keymap.c
+++ keyboards/ferris/keymaps/colemakDH/keymap.c
@@ -59,32 +59,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,         KC_W,        KC_F,        KC_P,        KC_B,          KC_J,    KC_L,        KC_U,        KC_Y,        KC_QUOT,
     WIN_T(KC_A),  ALT_T(KC_R), CTL_T(KC_S), SFT_T(KC_T), KC_G,          KC_M,    SFT_T(KC_N), CTL_T(KC_E), ALT_T(KC_I), WIN_T(KC_O),
     KC_Z,    KC_X,     KC_C,    KC_D,    KC_V,                          KC_K,    KC_H,        KC_COMM,     KC_DOT,      KC_SLSH,
-                         LT(_SYMB, KC_ENT), LT(_NUMB, KC_BSPC),         LT(_FUNC, KC_SPC), LT(_NAVG, KC_TAB)
+                         LT(_NUMB, KC_BSPC), LT(_SYMB, KC_ENT),         LT(_NAVG, KC_TAB), LT(_FUNC, KC_SPC)
   ),
 
   [_SYMB] = LAYOUT(
-    QK_BOOT,    ____,    ____,    ____,   ____,                      KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,
+    ____,    ____,    ____,    ____,   ____,                      KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,
     KC_LWIN, KC_LALT, KC_LCTL, KC_LSFT, KC_ESC,                      KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_COLN,
        ____, KC_RALT,    ____,  KC_DEL,   ____,                      KC_PIPE, KC_EXLM, KC_AT,   KC_HASH, KC_TILD,
                                          ____,   ____,     KC_UNDS, KC_RPRN
   ),
 
   [_NUMB] = LAYOUT(
-       ____,    ____,    ____,    ____,   ____,                     KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,
+       QK_BOOT,    ____,    ____,    ____,   ____,                     KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,
     KC_LWIN, KC_LALT, KC_LCTL, KC_LSFT, KC_ESC,                      KC_EQL, KC_4, KC_5, KC_6, KC_SCLN,
        ____,    ____,    ____,  KC_DOT,   ____,                     KC_BSLS, KC_1, KC_2, KC_3,  KC_GRV,
                                           ____,   ____,    KC_MINS, KC_0
   ),
 
   [_NAVG] = LAYOUT(
-       KC_PGUP,    KC_HOME,      KC_UP,     KC_END,      KC_INS,            ____,       ____,    ____,    ____, QK_BOOT,
+       KC_PGUP,    KC_HOME,      KC_UP,     KC_END,      KC_INS,            ____,       ____,    ____,    ____, ____,
        KC_PGDN,    KC_LEFT,    KC_DOWN,    KC_RGHT,     KC_CAPS,          KC_APP,    KC_LSFT, KC_LCTL, KC_LALT, KC_LWIN,
     LCTL(KC_Z),  LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_Y),            ____, SGUI(KC_S),    ____, KC_RALT,    ____,
                                         KC_BSPC, KC_ENT,              ____, ____
   ),
 
   [_FUNC] = LAYOUT(
-    KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,                      ____,       ____,    ____,    ____,    ____,
+    KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,                      ____,       ____,    ____,    ____,    QK_BOOT,
     KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL,                      ____,    KC_LSFT, KC_LCTL, KC_LALT, KC_LWIN,
     KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS,                      ____, KC_KB_MUTE,    ____, KC_RALT,    ____,
                                  KC_BSPC, KC_ENT,        ____, ____
